554207de8ff6021b97ca9f83f4a0af80430e35c4
HengHong Lee <henghong.lee@gmail.com>
2020-04-20 00:03:56 +0800

    modify seed.js

--- seed/seed.js
+++ seed/seed.js
@@ -9,9 +9,9 @@ export function updateDataIfNeeded() {
     .on('data', row => {
       var employee = new Employee({ email: row[0], slack_token: row[1] });
       employee.save((err, res) => {
+        if (err) { console.log(err) }
         if (res) {
-          console.log('Updating...');
-          console.log(res);
+          console.log(`Updating... ${res.email} - ${res.slack_token}`);
         }
       });
     });
